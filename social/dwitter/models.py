3a80cb937baef990bfd049412926767689960140
xunruan <xunruan@icloud.com>
2022-08-13 18:37:41 -0400

    Use decorator to send post_save signal

--- social/dwitter/models.py
+++ social/dwitter/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Profile(models.Model):
@@ -16,14 +17,13 @@ class Profile(models.Model):
         return self.user.username
 
 
+@receiver(post_save, sender=User)   # associate the post_save with the events related to the User model
 def create_profile(sender, instance, created, **kwargs):
 
     if created:
         user_profile = Profile(user=instance)   # create a new profile and set the user to the user that we are saving
         user_profile.save()     # Save the profile for it to have an id in the database
         user_profile.follows.add(instance.profile.id)
-        # user_profile.follows.set([instance.profile.id])  # set the follows property of the profile to the current user instance
         user_profile.save()  # Save the profile to the database
 
 
-post_save.connect(create_profile, sender=User)  # Connect a model's saving action to trigger a function
