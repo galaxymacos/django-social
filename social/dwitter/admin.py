4c8f160d74344cbe5e988c2f81d8919e5ff9fc0f
xunruan <xunruan@icloud.com>
2022-08-13 16:50:53 -0400

    Create a profile when saving a new user
    post_save hook

--- social/dwitter/admin.py
+++ social/dwitter/admin.py
@@ -4,12 +4,17 @@ from django.contrib.auth.models import Group, User
 from dwitter.models import Profile
 
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
 class UserAdmin(admin.ModelAdmin):
     model = User
     fields = ["username"]
+    inlines = [ProfileInline]
 
 
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
 admin.site.unregister(Group)
-admin.site.register(Profile)
+# Remove (because now we can edit Profile in UserAdmin): admin.site.register(Profile)
--- social/dwitter/models.py
+++ social/dwitter/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -7,6 +8,18 @@ class Profile(models.Model):
     follows = models.ManyToManyField(
         "self",
         related_name="followed_by",
-        symmetrical=False,
+        symmetrical=False,  # Users don't have the mutual following with the one who follow them
         blank=True
     )
+
+    def __str__(self):
+        return self.user.username
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+
+post_save.connect(create_profile, sender=User)  # Connect a model's saving action to trigger a function
