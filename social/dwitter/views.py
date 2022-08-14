a9c6968563a09b05c7f59505f662cc3656f8a274
xunruan <xunruan@icloud.com>
2022-08-14 13:37:28 -0400

    filter current user data

--- social/dwitter/views.py
+++ social/dwitter/views.py
@@ -10,3 +10,8 @@ def dashboard(request):
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
+
+
+def profile(request, pk):
+    profile_object = Profile.objects.get(pk=pk)  # The first pk can be id
+    return render(request, "dwitter/profile.html", {"profile": profile_object})
