ba2cc06674b5c371d74940a6845c8310288a5b4f
xunruan <xunruan@icloud.com>
2022-08-14 23:44:12 -0400

    send and process post request

--- social/dwitter/views.py
+++ social/dwitter/views.py
@@ -13,5 +13,19 @@ def profile_list(request):
 
 
 def profile(request, pk):
+    # Visit the profile page with the id = pk
+    if not hasattr(request.user, 'profile'):
+        missing_profile = Profile(user=request.user)
+        missing_profile.save()
+
     profile_object = Profile.objects.get(pk=pk)  # The first pk can be id
+    if request.method == "POST":    # submit the data to the same page
+        current_user_profile = request.user.profile
+        data = request.POST
+        action = data.get("follow")
+        if action == "follow":
+            current_user_profile.follows.add(profile_object)
+        elif action == "unfollow":
+            current_user_profile.follows.remove(profile_object)
+        current_user_profile.save()
     return render(request, "dwitter/profile.html", {"profile": profile_object})
